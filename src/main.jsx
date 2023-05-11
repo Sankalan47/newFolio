95ad271054d1bb8255a09031ff9a33e73c7117d3
sankalan <sankalandasgupta@gmail.com>
2023-05-11 18:27:37 +0530

    revert

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { ColorModeScript } from "@chakra-ui/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App.jsx";
